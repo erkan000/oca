f9cbda273aba3ac6c62fe273800b644d0dbbe903
Erkan <erkan000@hotmail.com>
2021-04-02 17:18:37 +0300

    operators

--- src/main/java/chap2/Operators.java
+++ src/main/java/chap2/Operators.java
@@ -4,16 +4,19 @@ public class Operators {
 	
 	public static void main(String[] args) {
 		
-		byte x = 6;
-		byte y = 1;
+		int x = 6;
+		int y = 1;
 		System.out.println("X sayısı " + Integer.toString( x		, 2));
 		System.out.println("Sağa     " + Integer.toString( x>>1 	, 2));
 		System.out.println("Sola     " + Integer.toString( x<<1 	, 2));
-		System.out.println("Bi tersi " + Integer.toString( ~x 		, 2));
+		System.out.println("Bi tersi " + Integer.toString( ~x 		, 2));  // ters demek bit tersi değil, 0 ı da katarak negatif tersi
+		
+		System.out.println("--------");
 		
 		System.out.println("Y sayısı " + Integer.toString( y		, 2));
-		System.out.println("x AND Y  " + Integer.toString( x&y 		, 2));
-		System.out.println("x OR  Y  " + Integer.toString( x|y 		, 2));
+		System.out.println("x AND y  " + Integer.toString( x&y 		, 2));
+		System.out.println("x OR  y  " + Integer.toString( x|y 		, 2));
+		System.out.println("x XOR  y " + Integer.toString( x^y 		, 2));
 		
 
 		/**
